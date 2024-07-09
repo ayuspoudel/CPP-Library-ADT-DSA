fe362eae2885ed785b685c82d19b87acd6ee5324
Ayush Poudel <ayushpoudel@usf.edu>
2024-07-09 08:39:31 -0400

    Modify Recursive BST Search

--- Binary-Search-Tree/bst_search_iteration.cpp
+++ Binary-Search-Tree/bst_search_iteration.cpp
@@ -3,35 +3,35 @@
 #include <stack>
 
 class BNode {
-public:
-    BNode() : elem(-1), left(nullptr), right(nullptr) {}
-    BNode(int n) : elem(n), left(nullptr), right(nullptr) {}
-    ~BNode() {}
-    void print() { std::cout << this->elem << std::endl; }
+	public:
+		BNode() : elem(-1), left(nullptr), right(nullptr) {}
+		BNode(int n) : elem(n), left(nullptr), right(nullptr) {}
+		~BNode() {}
+		void print() { std::cout << this->elem << std::endl; }
 
-private:
-    int elem;
-    BNode* left;
-    BNode* right;
-    friend class BSTree;
+	private:
+		int elem;
+		BNode* left;
+		BNode* right;
+		friend class BSTree;
 };
 
 class BSTree {
-public:
-    BSTree() : root(nullptr) {}
-    BSTree(int n) : root(new BNode(n)) {}
-    ~BSTree() {}
-    void insert(int n);
-    BNode* search(int n);
-    void preorderTraversal();
-    BNode* getRoot() { return this->root; }
-	void inorderTraversal();
+	public:
+		BSTree() : root(nullptr) {}
+		BSTree(int n) : root(new BNode(n)) {}
+		~BSTree() {}
+		void insert(int n);
+		BNode* search(int n);
+		void preorderTraversal();
+		BNode* getRoot() { return this->root; }
+		void inorderTraversal();
 
-private:
-    BNode* root;
-    BNode* insert(BNode* node, int n);
-    void preorderTraversal(BNode* root);
-	void inorderTraversal(BNode* root);
+	private:
+		BNode* root;
+		BNode* insert(BNode* node, int n);
+		void preorderTraversal(BNode* root);
+		void inorderTraversal(BNode* root);
 };
 
 BNode* BSTree::search(int n){
@@ -52,17 +52,47 @@ BNode* BSTree::search(int n){
 }
 
 BNode* BSTree::insert(BNode* node, int n) {
-    if (node == nullptr) {
-        return new BNode(n);
-    }
-    if (n > node->elem) {
-        node->right = insert(node->right, n);
-    } else if (n < node->elem) {
-        node->left = insert(node->left, n);
-    }
-    return node;
+	BNode* newNode = new BNode(n);
+	if(node ==nullptr){
+		return newNode;
+	}
+	BNode* slow = nullptr;
+	BNode* fast = node;
+	while(fast!=nullptr){
+		slow = fast;
+		if(fast->elem<n){
+			fast = fast->right;
+		}
+		else if(fast->elem>n){
+			fast = fast->left;
+		}
+		else{
+			std::cout<<"The value is already in the tree"<<std::endl;
+			return node;
+		}
+	}
+	if(n<slow->elem){
+		slow->left = newNode;
+	}
+	else{
+		slow->right = newNode;
+	}
+	return node;
 }
 
+// BNode* BSTree::insert(BNode* node, int n){
+// 	BNode* newNode = new BNode(n);
+// 	if(node == nullptr){
+// 		return newNode;
+// 	}
+// 	else if(node->elem >n){
+// 		node->left = insert(node->left,n);
+// 	}
+// 	else if(node->elem<n){
+// 		node->right = insert(node->right, n);
+// 	}
+// 	return node;
+// }
 void BSTree::insert(int n) {
     root = insert(root, n);
 }
