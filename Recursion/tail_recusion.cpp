9c14f4c26bfb92070d5fc7235fad12de566bd0a4
Ayush Poudel <ayushpoudel@usf.edu>
2024-08-22 14:29:14 -0400

    Add tail recursion program

---
+++ Recursion/tail_recusion.cpp
@@ -0,0 +1,15 @@
+
+#include <cstdlib>
+#include <iostream>
+
+void func1(int n){
+    if(n>0){
+        func1(n-1);
+        std::cout<<n<<" ";
+    }
+}
+
+int main(){
+    func1(3);
+    return EXIT_SUCCESS;
+}
